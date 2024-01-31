074cc1a824a1f61ce72fd9e30c10d8f1edef243e
Olaoluwa <olaoluwaraji1999@gmail.com>
2024-01-31 05:40:50 +0100

    Improved and tested the code. The control path comes next

--- EP4CE6_Projects/P3_7_Segment_DHT22/src/lib/one_wire.vhd
+++ EP4CE6_Projects/P3_7_Segment_DHT22/src/lib/one_wire.vhd
@@ -9,18 +9,21 @@ entity one_wire is
 		  param: in std_logic; --Select Celsius or Humidity
 		  io: inout std_logic; --One wire data line (High Impedance)
 		  data_out: out std_logic_vector(15 downto 0);
-		  done: out std_logic);
+		  done: out std_logic;
+		  valid: out std_logic);
 end one_wire;
 
 architecture one_wire_rtl of one_wire is
 	--Start pulse: about 18ms
 	--Pre-ack pulse: about 40uS
-	--Bit low pulse: about 50uS
+	--Bit low pulse: Every data bit starts with a low pulse of about 50uS
+	--Max bit pulse(M): Bit = '1' if high pulse of data bit exceeds M, else '0'.
 	--ACK period: about 160uS
 	--Bit 0: 26-28uS after initial 50uS for each DHT22 bit
 	--Bit 1: 69-71uS after initial 50uS for each DHT22 bit
 	constant start_pulse: integer := 899_999;
 	constant pre_ack_pulse: integer := 1999;
+	constant max_bit_pulse: integer := 2499;
 	constant bit_low_pulse: integer := 2499;
 	constant ack_period: integer := 7999;
 	type one_wire_state is (ST_IDLE, ST_START, ST_PRE_ACK, ST_ACK, 
@@ -30,17 +33,18 @@ architecture one_wire_rtl of one_wire is
 	signal bit_count: integer range 0 to 40; --40 data bits + redundant bit
 	signal clks: unsigned(19 downto 0);
 	signal clk_stamp: unsigned(19 downto 0);
-	signal cnt_en: std_logic;
-	signal one_wire_buff: std_logic_vector(0 to 39);
-	signal read_io: std_logic;
-	signal old_io_state: std_logic;
+	signal count_en: std_logic;
+	signal data_buff: std_logic_vector(0 to 39);
+	signal new_io: std_logic;
+	signal old_io: std_logic;
+	signal check_sum: unsigned(7 downto 0);
 begin
 	clock_counter: process(rst_n,clk)
 	begin
 		if rst_n = '0' then
 			clks <= (others => '0');
 		elsif rising_edge(clk) then
-			if cnt_en = '1' then
+			if count_en = '1' then
 				clks <= clks + 1;
 			else
 				clks <= (others => '0');
@@ -48,7 +52,7 @@ begin
 		end if;
 	end process;	
 	
-	state_transition: process(state,en,clks,bit_count,read_io,old_io_state)
+	state_transition: process(state,en,clks,bit_count,new_io,old_io)
 	begin
 		next_state <= state;
 		case state is
@@ -74,7 +78,7 @@ begin
 				if bit_count = 40 then
 					next_state <= ST_DONE;
 				else
-					if read_io = old_io_state then
+					if new_io = old_io then
 						next_state <= ST_SAMPLE;
 					end if;
 				end if;
@@ -83,6 +87,9 @@ begin
 					next_state <= ST_CHECK;
 				end if;			
 			when ST_CHECK =>
+				if en = '0' then
+					next_state <= ST_IDLE;
+				end if;
 		end case;
 	end process;
 	
@@ -94,42 +101,42 @@ begin
 			state <= next_state;
 		end if;
 	end process;
-
-	data_selector: process(param,one_wire_buff)
-	begin
-		if param = '0' then
-			data_out <= one_wire_buff(0 to 15);
-		else
-			data_out <= one_wire_buff(16 to 31);
-		end if;
-	end process;
 	
-	combinational_output: process(state,clks,bit_count)
+	combinational_outputs: process(state,clks,bit_count,check_sum,data_buff)
 	begin
-		cnt_en <= '0';
+		count_en <= '0';
 		done <= '0';
+		valid <= '0';
 		case state is
 			when ST_IDLE =>
 				if en = '1' then
+					count_en <= '1';
 				end if;
 			when ST_START =>
 				if clks /= start_pulse then
-					cnt_en <= '1';
+					count_en <= '1';
 				end if;
 			when ST_PRE_ACK =>
 				if clks /= pre_ack_pulse then
-					cnt_en <= '1';
+					count_en <= '1';
 				end if;
-			when ST_ACK | ST_SAMPLE =>
-				cnt_en <= '1';
+			when ST_ACK =>
+				if clks /= ack_period then
+					count_en <= '1';
+				end if;
+			when ST_SAMPLE =>
+				count_en <= '1';
 			when ST_PROCESS_DATA =>
 				if bit_count /= 40 then
-					cnt_en <= '1';
+					count_en <= '1';
 				end if;
 			when ST_DONE =>
-				cnt_en <= '1';
+				count_en <= '1';
 				done <= '1';
-			when others =>
+			when ST_CHECK =>
+				if check_sum = unsigned(data_buff(32 to 39)) then
+					valid <= '1';
+				end if;
 		end case;
 	end process;
 	
@@ -138,10 +145,10 @@ begin
 		if rst_n = '0' then
 			io <= 'Z';
 			bit_count <= 0;
-			one_wire_buff <= (others => '0');
-			read_io <= '0';
-			old_io_state <= '0';
 			clk_stamp <= (others => '0');
+			data_buff <= (others => '0');
+			new_io <= '0';
+			old_io <= '0';
 		elsif rising_edge(clk) then
 			case state is
 				when ST_IDLE =>
@@ -153,29 +160,44 @@ begin
 						io <= 'Z';
 					end if;
 				when ST_SAMPLE =>
-					read_io <= io;
+					new_io <= io;
 				when ST_PROCESS_DATA =>
-					if read_io = '1' and old_io_state = '0' then
-						old_io_state <= '1';
+					if new_io = '1' and old_io = '0' then
+						old_io <= '1';
 						clk_stamp <= clks;
-					elsif read_io = '0' and old_io_state = '1' then
-						if clks - clk_stamp >= 2499 then
-							one_wire_buff(bit_count) <= '1';
+					elsif new_io = '0' and old_io = '1' then
+						if clks - clk_stamp >= max_bit_pulse then
+							data_buff(bit_count) <= '1';
 						else
-							one_wire_buff(bit_count) <= '0';
+							data_buff(bit_count) <= '0';
 						end if;
 						bit_count <= bit_count + 1;
-						old_io_state <= '0';						
+						old_io <= '0';						
 					end if;			
 				when ST_DONE =>
 					bit_count <= 0;
 					if clks >= bit_low_pulse then
 						io <= 'Z'; --Release one-wire bus
 					end if;
-				when ST_CHECK =>
 				when others =>
 			end case;
 		end if;
 	end process;
+	
+	output_data_selector: process(param,data_buff)
+	begin
+		if param = '0' then
+			data_out <= data_buff(0 to 15); --Humidity
+		else
+			data_out <= data_buff(16 to 31); --Temperature
+		end if;
+	end process;
+	
+	--Check sum = Humidity integral + Humidity decimal +
+	--            Temperature integral + Temperature decimal
+	check_sum_calc: check_sum <= unsigned(data_buff(0 to 7))   + 
+										  unsigned(data_buff(8 to 15))  +
+										  unsigned(data_buff(16 to 23)) +
+										  unsigned(data_buff(24 to 31));
 	
 end architecture one_wire_rtl;
--- EP4CE6_Projects/P3_7_Segment_DHT22/src/weather_sensor.vhd
+++ EP4CE6_Projects/P3_7_Segment_DHT22/src/weather_sensor.vhd
@@ -7,41 +7,53 @@ entity weather_sensor is
 	port(rst_n: in std_logic;
 		  clk: in std_logic;
 		  io: inout std_logic;
+		  led: out std_logic; --to indicate valid DHT22 sensor data
 		  dp: out std_logic;
 		  seg: out std_logic_vector(6 downto 0);
 		  sel: out std_logic_vector(3 downto 0));
 end weather_sensor;
 
 architecture weather_sensor_rtl of weather_sensor is
+	signal en: std_logic;
 	signal dec: std_logic_vector(15 downto 0);
 	signal bcd: std_logic_vector(11 downto 0);
-	signal dht22_done: std_logic;
 	signal done: std_logic;
+	signal conversion_done: std_logic;
+	signal valid: std_logic;
 	signal param: std_logic;
-	signal count: unsigned(31 downto 0);
+	--signal count: unsigned(31 downto 0);
 begin
-	process(rst_n,clk)
-	begin
-		if rst_n = '0' then
-			count <= (others => '0');
-		elsif rising_edge(clk) then
-			if count = to_unsigned(99_999_999,count'length) then
-				count <= (others => '0');
-				param <= not param;
-			else
-				count <= count + 1;
-			end if;
-		end if;
-	end process;
+--	control_path: process(rst_n,clk)
+--	begin
+--		if rst_n = '0' then
+--		elsif rising_edge(clk) then
+--		end if;
+--	end process;
 	
+--	process(rst_n,clk)
+--	begin
+--		if rst_n = '0' then
+--			count <= (others => '0');
+--			en <= '0';
+--		elsif rising_edge(clk) then
+--			if count = to_unsigned(99_999_999,count'length) then
+--				count <= (others => '0');
+--				en <= not conversion_done;
+--			else
+--				count <= count + 1;
+--			end if;
+--		end if;
+--	end process;
+	
+	led <= not valid;
 	dht22_one_wire: entity work.one_wire(one_wire_rtl)
-	port map(rst_n => rst_n, clk => clk, en => '1', param => '0',
-				io => io, data_out => dec, done => dht22_done);
+	port map(rst_n => rst_n, clk => clk, en => '1', param => param,
+				io => io, data_out => dec, done => done, valid => valid);
 	
 	dht22_data_bcd: entity work.dec_to_bcd(dec_to_bcd_rtl)
 	generic map(data_width => 16)
-	port map(rst_n => rst_n, clk => clk, en => dht22_done, dec => dec,
-				bcd => bcd, done => done);
+	port map(rst_n => rst_n, clk => clk, en => done, dec => dec,
+				bcd => bcd, done => conversion_done);
 	
 	display: entity work.seg_display(seg_display_rtl)
 	port map(rst_n => rst_n, clk => clk, bcd => bcd, param => param,
