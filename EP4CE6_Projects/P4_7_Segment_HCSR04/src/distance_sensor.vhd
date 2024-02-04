103c668e19d5e1a64c5a4033a6856bf776db06d7
Olaoluwa <olaoluwaraji1999@gmail.com>
2024-02-04 21:59:05 +0100

    Module to interface ultrasonic sensor with EP4CE6 FPGA

---
+++ EP4CE6_Projects/P4_7_Segment_HCSR04/src/distance_sensor.vhd
@@ -0,0 +1,74 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+library work;
+
+--Project: Distance measurement and display using HCSR04 and 7-segment display.
+-- Distances are measured in centimeters.
+
+--Details:
+--1. "Active low" reset and 7-segment display
+--2. 50MHz system clock
+--3. The design is divided into different parts:
+--	  a. ROM  
+--   b. Decimal to BCD converter
+--   c. HCSR04 sensor interface
+--   d. Shift register and counters
+--   e. Other combinational logic
+--   f. Control paths
+
+entity distance_sensor is
+	port(rst_n: in std_logic;
+		  clk: in std_logic;
+		  echo: in std_logic;
+		  trig: out std_logic;
+		  dp: out std_logic;
+		  seg: out std_logic_vector(6 downto 0);
+		  sel: out std_logic_vector(3 downto 0));
+end distance_sensor;
+
+architecture distance_sensor_rtl of distance_sensor is
+	constant PULSE_TO_CM: integer := 172;
+	constant BIT_WIDTH: integer := 8;
+	signal en: std_logic;
+	signal done: std_logic;
+	signal conversion_done: std_logic;
+	signal distance: unsigned(23 downto 0);
+	signal dec: std_logic_vector(23 downto 0);
+	signal bcd: std_logic_vector(15 downto 0);
+	signal pulse: std_logic_vector(15 downto 0);
+	signal clks: unsigned(27 downto 0);
+begin
+	--Periodically trigger the HCSR04 sensor to obtain its readings
+	control_path: process(rst_n,clk)
+	begin
+		if rst_n = '0' then
+			clks <= (others => '0');
+			en <= '1';
+		elsif rising_edge(clk) then
+			if clks = to_unsigned(99_999_999,clks'length) then
+				clks <= (others => '0');
+				en <= not conversion_done;
+			else
+				clks <= clks + 1;
+			end if;
+		end if;
+	end process;
+
+	hcsr04_inst: entity work.hcsr04(hcsr04_rtl)
+	port map(rst_n => rst_n, clk => clk, en => en, echo => echo,
+		      trig => trig, pulse => pulse, done => done);
+	
+	distance <= unsigned(pulse) * to_unsigned(PULSE_TO_CM,BIT_WIDTH); 
+	dec <= std_logic_vector(distance);
+	
+	dec_to_bcd_inst: entity work.dec_to_bcd(dec_to_bcd_rtl)
+	port map(rst_n => rst_n, clk => clk, en => done, dec => dec,
+				bcd => bcd, done => conversion_done);
+	
+	display_inst: entity work.seg_display(seg_display_rtl)
+	port map(rst_n => rst_n, clk => clk, bcd => bcd,
+				dp => dp, seg => seg, sel => sel);
+				
+end distance_sensor_rtl;
+
---
+++ EP4CE6_Projects/P4_7_Segment_HCSR04/src/lib/hcsr04.vhd
@@ -0,0 +1,156 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+library work;
+
+--Module for interfacing the FPGA with the HCSR04 sensor.
+
+entity hcsr04 is
+	port(rst_n: in std_logic;
+	     clk: in std_logic;
+		  en: in std_logic;
+		  echo: in std_logic;
+		  trig: out std_logic;
+		  pulse: out std_logic_vector(15 downto 0);
+		  done: out std_logic);
+end hcsr04;
+
+architecture hcsr04_rtl of hcsr04 is
+	--50MHz clock: 50 cycles = 1uS
+	constant CLKS_PER_US: integer := 50;
+	constant TRIG_PULSE: integer := 15; --15uS trigger pulse
+	type fsm is (ST_IDLE, ST_TRIG, ST_SAMPLE, ST_MEASURE, ST_DONE);
+	signal state: fsm;
+	signal next_state: fsm;
+	signal cnt_en: std_logic;
+	signal cnt_en_reg: std_logic;
+	signal clks: unsigned(5 downto 0);
+	signal micro_clks: unsigned(15 downto 0);
+	signal new_io: std_logic; --current sample of 'echo' signal
+	signal new_io_reg: std_logic;
+	signal old_io: std_logic; --previous sample of 'echo' signal
+	signal old_io_reg: std_logic;
+	signal pos_edge: unsigned(15 downto 0);
+	signal pos_reg: unsigned(15 downto 0);
+begin
+	counters: process(rst_n,clk)
+	begin
+		if rst_n = '0' then
+			clks <= (others => '0');
+			micro_clks <= (others => '0');
+		elsif rising_edge(clk) then
+			if cnt_en_reg = '1' then
+				if clks = to_unsigned(CLKS_PER_US - 1,clks'length) then
+					clks <= (others => '0');
+					micro_clks <= micro_clks + 1;
+				else
+					clks <= clks + 1;
+				end if;
+			else
+				clks <= (others => '0');
+				micro_clks <= (others => '0');
+			end if;
+		end if;
+	end process;
+	
+	next_state_logic: process(state,en,micro_clks,new_io_reg,old_io_reg)
+	begin
+		next_state <= state;
+		case state is
+			when ST_IDLE =>
+				if en = '1' then
+					next_state <= ST_TRIG;
+				end if;
+			when ST_TRIG =>
+				if micro_clks = to_unsigned(TRIG_PULSE - 1,micro_clks'length) then
+					next_state <= ST_SAMPLE;
+				end if;
+			when ST_SAMPLE =>
+				next_state <= ST_MEASURE;
+			when ST_MEASURE =>
+				if new_io_reg = old_io_reg then
+					next_state <= ST_SAMPLE;
+				elsif new_io_reg = '0' and old_io_reg = '1' then
+					next_state <= ST_DONE;
+				end if;
+			when ST_DONE =>
+				if en = '0' then
+					next_state <= ST_IDLE;
+				end if;
+		end case;
+	end process;
+	
+	state_register: process(rst_n,clk)
+	begin
+		if rst_n = '0' then
+			state <= ST_IDLE;
+		elsif rising_edge(clk) then
+			state <= next_state;
+		end if;
+	end process;
+	
+	combinational_outputs: 
+	process(state,en,micro_clks,echo,new_io_reg,old_io_reg,pos_reg,cnt_en_reg)
+	begin
+		trig <= '0';
+		cnt_en <= cnt_en_reg;
+		new_io <= new_io_reg;
+		old_io <= old_io_reg;
+		pos_edge <= pos_reg;
+		done <= '0';
+		case state is
+			when ST_IDLE =>
+				if en = '1' then
+					trig <= '1';
+					cnt_en <= '1';
+				else
+					cnt_en <= '0';
+				end if;
+			when ST_TRIG =>
+				if micro_clks = to_unsigned(TRIG_PULSE - 1,micro_clks'length) then
+					cnt_en <= '0';
+				else
+					trig <= '1';
+					cnt_en <= '1';
+				end if;
+			when ST_SAMPLE => 
+				new_io <= echo;
+				if old_io_reg = '1' then
+					cnt_en <= '1';
+				else
+					cnt_en <= '0';
+				end if;
+			when ST_MEASURE =>
+				if new_io_reg = '1' and old_io_reg = '0' then
+					cnt_en <= '1';
+					old_io <= '1';
+					pos_edge <= micro_clks;
+				elsif new_io_reg = '0' and old_io_reg = '1' then
+					cnt_en <= '0';
+					old_io <= '0';
+					pos_edge <= micro_clks - pos_reg;--pulse width (microseconds)
+				end if;
+			when ST_DONE =>
+				done <= '1';
+		end case;
+	end process;
+	
+	pulse <= std_logic_vector(pos_reg);
+	
+	registered_outputs: process(rst_n,clk)
+	begin
+		if rst_n = '0' then
+			new_io_reg <= '0';
+			old_io_reg <= '0';
+			pos_reg <= (others => '0');
+			cnt_en_reg <= '0';
+		elsif rising_edge(clk) then
+			new_io_reg <= new_io;
+			old_io_reg <= old_io;
+			pos_reg <= pos_edge;
+			cnt_en_reg <= cnt_en;
+		end if;
+	end process;
+	
+end hcsr04_rtl;
+
