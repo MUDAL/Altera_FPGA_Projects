b166e03f85066195891012d48eea57154c6e5664
Olaoluwa <olaoluwaraji1999@gmail.com>
2024-07-16 11:21:20 +0100

    Added README file and modified some settings for ModelSim

---
+++ EP4CE6_Projects/P12_Pong/VHDL/simulation/testbench/button_logic_tb.vhd
@@ -0,0 +1,74 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+library work;
+use work.pack_tb.all;
+
+entity button_logic_tb is
+end button_logic_tb;
+
+architecture button_logic_behav of button_logic_tb is
+   constant CLK_DIV: integer := 10_000;
+   constant SLOW_CLK_PERIOD: time := CLK_DIV * CLK_PERIOD;
+   constant SLOW_CLK_FREQ: integer := 50_000_000 / CLK_DIV;
+   constant DELAY_MS: integer := 5;
+   constant UNIT_DELAY: time := 1 ms;
+   signal rst_n: std_logic;
+   signal clk: std_logic := '0';
+   signal btn_in: std_logic := '1';
+   signal btn_out: std_logic;
+   signal end_test: std_logic := '0';
+begin
+   uut: entity work.button_logic(button_logic_rtl)
+   generic map(CLK_FREQ => SLOW_CLK_FREQ,
+               DELAY_MS => DELAY_MS)
+   port map(rst_n => rst_n,
+            clk => clk,
+            btn_in => btn_in,
+            btn_out => btn_out);
+   
+   reset: rst_n <= '0', '1' after 2 * SLOW_CLK_PERIOD;
+   
+   clock_generation: process
+   begin
+      wait for SLOW_CLK_PERIOD / 2;
+      clk <= not clk;
+   end process; 
+   
+   stimuli: process
+   begin
+      wait until rst_n = '1';
+      wait until rising_edge(clk);
+      wait until rising_edge(clk);
+      -- Noisy button press
+      for i in 0 to 9 loop
+         btn_in <= not btn_in;
+         wait until rising_edge(clk);
+      end loop;
+      btn_in <= '0';
+      wait for UNIT_DELAY * DELAY_MS;
+      
+      -- Noisy button release
+      for i in 0 to 9 loop
+         btn_in <= not btn_in;
+         wait until rising_edge(clk);
+      end loop;
+      btn_in <= '1';
+      wait for UNIT_DELAY * DELAY_MS;
+      end_test <= '1';
+      wait;
+   end process;
+   
+   output_report: process
+   begin
+      wait until rst_n = '1';
+      wait until btn_out = '0';
+      report "Button pressed";
+      
+      wait until btn_out = '1';
+      report "Button released";
+      wait until end_test = '1';
+      assert false report "Simulation done" severity failure;
+      wait;
+   end process;
+end button_logic_behav;
---
+++ EP4CE6_Projects/P12_Pong/VHDL/simulation/testbench/score_logic_tb.vhd
@@ -0,0 +1,89 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+library work;
+use work.pack_tb.all;
+
+entity score_logic_tb is
+end score_logic_tb;
+
+architecture score_logic_behav of score_logic_tb is
+   constant RIGHT_CRASH: std_logic_vector(1 downto 0) := "01";
+   constant LEFT_CRASH: std_logic_vector(1 downto 0) := "10";
+   constant NO_CRASH: std_logic_vector(1 downto 0) := "00";
+   signal rst_n: std_logic;
+   signal clk: std_logic := '0';
+   signal crash: std_logic_vector(1 downto 0) := "00";
+   signal score1_bcd_tens: std_logic_vector(3 downto 0);
+   signal score1_bcd_ones: std_logic_vector(3 downto 0);
+   signal score2_bcd_tens: std_logic_vector(3 downto 0);
+   signal score2_bcd_ones: std_logic_vector(3 downto 0);
+   signal valid_scores: std_logic;
+   signal end_test: std_logic := '0';
+begin
+   uut: entity work.score_logic(score_logic_rtl)
+   generic map(MAX_SCORE => 11)
+   port map(rst_n => rst_n,
+            clk => clk,
+            crash => crash,
+            score1_bcd_tens => score1_bcd_tens,
+            score1_bcd_ones => score1_bcd_ones,
+            score2_bcd_tens => score2_bcd_tens,
+            score2_bcd_ones => score2_bcd_ones,
+            valid_scores => valid_scores);
+         
+   reset: rst_n <= '0', '1' after 2 * CLK_PERIOD;
+   
+   clock_generation: process
+   begin
+      wait for CLK_PERIOD / 2;
+      clk <= not clk;
+   end process; 
+      
+   stimuli: process
+      constant DELAY_CLKS: integer := 48;
+      ---------------------------------------------------------------
+      procedure inject(constant BORDER: in std_logic_vector(1 downto 0);
+                       constant CYCLES: in integer) is
+      begin
+         crash <= BORDER; -- Left or Right
+         for i in 0 to CYCLES - 1 loop
+            wait until rising_edge(clk);
+         end loop;
+      end procedure inject;
+      ---------------------------------------------------------------
+   begin
+      wait until rst_n = '1';
+      
+      report "[INFO]: Right crash";
+      inject(RIGHT_CRASH, DELAY_CLKS); 
+      
+      inject(NO_CRASH, DELAY_CLKS);
+      
+      report "[INFO]: Left crash";
+      inject(LEFT_CRASH, DELAY_CLKS); 
+      end_test <= '1';
+      wait; 
+   end process;
+   
+   output_report: process
+      variable score1_str_tens: string(1 to 1);
+      variable score1_str_ones: string(1 to 1);
+      variable score2_str_tens: string(1 to 1);
+      variable score2_str_ones: string(1 to 1);
+   begin
+      wait until rst_n = '1';
+      wait on crash;
+      while end_test = '0' loop
+         wait until valid_scores = '1';
+         score1_str_tens(1) := slv2char(score1_bcd_tens);
+         score1_str_ones(1) := slv2char(score1_bcd_ones);
+         score2_str_tens(1) := slv2char(score2_bcd_tens);
+         score2_str_ones(1) := slv2char(score2_bcd_ones);
+         report "Score 1: " & score1_str_tens & score1_str_ones & ", " &
+                "Score 2: " & score2_str_tens & score2_str_ones;
+      end loop;
+      assert false report "Simulation done" severity failure;
+      wait;    
+   end process;
+end score_logic_behav;
