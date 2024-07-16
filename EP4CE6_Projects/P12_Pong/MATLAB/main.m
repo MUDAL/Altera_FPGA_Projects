7410086684cc81b2877eb0caaf4a6f51aafbef59
Olaoluwa <olaoluwaraji1999@gmail.com>
2024-07-16 19:01:30 +0100

    Minor addition to the main.m script

--- EP4CE6_Projects/P12_Pong/MATLAB/main.m
+++ EP4CE6_Projects/P12_Pong/MATLAB/main.m
@@ -21,5 +21,8 @@ imshow(gamemenu_binary);
 figure
 imshow(gameover_binary);
 
-img_to_mif(gamemenu_gray,'game_menu.mif');
-img_to_mif(gameover_gray,'game_over.mif');
+img_to_mif(gamemenu_binary,'game_menu.mif');
+img_to_mif(gameover_binary,'game_over.mif');
+
+imwrite(gamemenu_binary,'gamemenu_inverted.jpg');
+imwrite(gameover_binary,'gameover_inverted.jpg');
